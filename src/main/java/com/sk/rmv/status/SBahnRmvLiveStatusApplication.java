cdeb9fe44bc71e95f59a64b174f8aa4b6150f117
Shishir Kumar <shishir.insane@gmail.com>
2019-05-12 18:41:43 +0200

    First Light

---
+++ src/main/java/com/sk/rmv/status/SBahnRmvLiveStatusApplication.java
@@ -0,0 +1,13 @@
+package com.sk.rmv.status;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SBahnRmvLiveStatusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SBahnRmvLiveStatusApplication.class, args);
+	}
+
+}
